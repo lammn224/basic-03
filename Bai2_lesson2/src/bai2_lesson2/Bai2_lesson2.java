a9756f1b70cf790b51656bd796760e1e3b82126a
lamnn224 <hoanglam2242001@gmail.com>
2019-09-30 01:03:31 +0700

    Bai2_lesson2

--- Bai2_lesson2/src/bai2_lesson2/Bai2_lesson2.java
+++ Bai2_lesson2/src/bai2_lesson2/Bai2_lesson2.java
@@ -28,7 +28,7 @@ public class Bai2_lesson2 {
             System.out.printf("Cac so nguyen to be hon %d la: \n", n);
             for (int i = 2; i < n; i++) {
                 count = 0;
-                for (int j = 1; j <= n; j++) {
+                for (int j = 1; j <= i; j++) {
                     if (i % j == 0) {
                         count++;
                     }
