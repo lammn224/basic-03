63e42bad09562283a604ff3714ea4e480f4ffcf5
lamnn224 <hoanglam2242001@gmail.com>
2019-10-06 00:37:53 +0700

    Bai5

---
+++ Homework_Lesson4/Bai5/src/bai5/Bai5.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class Bai5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        Scanner b = new Scanner(System.in);
+        String A, B;
+        System.out.print("Nhap vao chuoi thu nhat: ");
+        A = a.next();
+        System.out.print("Nhap vao chuoi thu hai: ");
+        B = b.next();
+        System.out.println(comparison(A, B));
+    }
+
+    public static int comparison(String a, String b) {
+        if (a.compareTo(b) > 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
