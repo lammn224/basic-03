6a3ad2f3792eee08ef8ad02f5629642887dd7972
lamnn224 <hoanglam2242001@gmail.com>
2019-10-09 23:42:37 +0700

    Homework_Lesson5

--- Homework_Lesson5/Bai2_Lesson5/src/bai2_lesson5/Bai2_Lesson5.java
+++ Homework_Lesson5/Bai2_Lesson5/src/bai2_lesson5/Bai2_Lesson5.java
@@ -19,8 +19,8 @@ public class Bai2_Lesson5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap vao so tien: ");
-        int n = sc.nextInt();
-        String s = Integer.toString(n);   // chuyển từ int sang dạng String
+        long n = sc.nextLong();
+        String s = Long.toString(n);   // chuyển từ int sang dạng String
         int X = s.length();
         if (X < 4) {
             System.out.println(n + "vnd");
@@ -29,20 +29,47 @@ public class Bai2_Lesson5 {
             String baso2 = s.substring(0, X - 3);
             String SoTien = baso2 + "," + baso1 + "vnd";
             System.out.println(SoTien);
-            
+
         } else if (X > 6 && X <= 9) {
             String baso1 = s.substring(X - 3, X);
             String baso2 = s.substring(X - 6, X - 3);
             String baso3 = s.substring(0, X - 6);
             String SoTien = baso3 + "," + baso2 + "," + baso1 + "vnd";
             System.out.println(SoTien);
-            
+
         } else if (X > 9 && X <= 12) {
             String baso1 = s.substring(X - 3, X);
             String baso2 = s.substring(X - 6, X - 3);
             String baso3 = s.substring(X - 9, X - 6);
             String baso4 = s.substring(0, X - 9);
             String SoTien = baso4 + "," + baso3 + "," + baso2 + "," + baso1 + "vnd";
+            System.out.println(SoTien);
+        } else if (X > 12 && X <= 15) {
+            String baso1 = s.substring(X - 3, X);
+            String baso2 = s.substring(X - 6, X - 3);
+            String baso3 = s.substring(X - 9, X - 6);
+            String baso4 = s.substring(X - 12, X - 9);
+            String baso5 = s.substring(0, X - 12);
+            String SoTien = baso5 + "," + baso4 + "," + baso3 + "," + baso2 + "," + baso1 + "vnd";
+            System.out.println(SoTien);
+        } else if (X > 15 && X <= 18) {
+            String baso1 = s.substring(X - 3, X);
+            String baso2 = s.substring(X - 6, X - 3);
+            String baso3 = s.substring(X - 9, X - 6);
+            String baso4 = s.substring(X - 12, X - 9);
+            String baso5 = s.substring(X - 15, X - 12);
+            String baso6 = s.substring(0, X - 15);
+            String SoTien = baso6 + "," + baso5 + "," + baso4 + "," + baso3 + "," + baso2 + "," + baso1 + "vnd";
+            System.out.println(SoTien);
+        } else if (X > 18 && X <= 21) {
+            String baso1 = s.substring(X - 3, X);
+            String baso2 = s.substring(X - 6, X - 3);
+            String baso3 = s.substring(X - 9, X - 6);
+            String baso4 = s.substring(X - 12, X - 9);
+            String baso5 = s.substring(X - 15, X - 12);
+            String baso6 = s.substring(X - 18, X - 15);
+            String baso7 = s.substring(0, X - 18);
+            String SoTien = baso7 + "," + baso6 + "," + baso5 + "," + baso4 + "," + baso3 + "," + baso2 + "," + baso1 + "vnd";
             System.out.println(SoTien);
         }
     }
